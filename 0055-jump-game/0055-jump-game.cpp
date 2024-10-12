9aa40906ed3399801ec5460cb714abca9fe5a724
Anmol Saini <143955945+AnmolSaini28@users.noreply.github.com>
2024-10-12 16:53:41 +0530

    Time: 43 ms (63.94%), Space: 51.2 MB (42.4%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxInd = 0;
+        for(int i=0 ; i<nums.size() ; i++){
+            if(maxInd < i) return false;
+            else{
+                maxInd = max(maxInd, i+nums[i]);
+            }
+        }
+        return true;
+    }
+};
