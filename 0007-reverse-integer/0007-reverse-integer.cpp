8f323208a1e90c8bde8f79c6762a7164d29ef5ea
Anmol Saini <143955945+AnmolSaini28@users.noreply.github.com>
2024-10-31 14:29:53 +0530

    Time: 0 ms (100%), Space: 8.4 MB (42.46%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int ans = 0;
+        while(x > 0 || x < 0){
+            int ld = x%10;
+            if((pow(2,31)-1)/10 < ans || -pow(2,31)/10 > ans){
+                return 0;
+            }
+            ans = ans*10+ld;
+            x = x/10;
+        }
+        return (int) ans;
+    }
+};
