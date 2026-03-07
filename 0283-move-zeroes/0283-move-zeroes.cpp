c147636ac3ae8a759e438ba3cf2bd30224ac3072
Anmol Saini <143955945+AnmolSaini28@users.noreply.github.com>
2026-03-07 20:47:45 +0530

    Time: 0 ms (100%), Space: 23.8 MB (81.62%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int lastNonZeroIndex = 0;
-        for(int i=0 ; i<nums.size() ; i++){
+        int n = nums.size();
+        int j = 0;
+        for(int i=0 ; i<n ; i++){
             if(nums[i] != 0){
-                swap(nums[i], nums[lastNonZeroIndex]);
-                lastNonZeroIndex++;
+                swap(nums[i],nums[j]);
+                j++;
             }
         }
     }
