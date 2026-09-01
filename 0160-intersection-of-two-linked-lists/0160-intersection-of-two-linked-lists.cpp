3814512a077b315ed23b2d98bcf8083649a197dc
Anmol Saini <143955945+AnmolSaini28@users.noreply.github.com>
2026-09-01 11:16:42 +0530

    Time: 56 ms (59.38%), Space: 24 MB (25.15%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -9,34 +9,21 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        int countA = 0;
-        int countB = 0;
-        ListNode *tempA = headA;
-        while(tempA != NULL){
-            countA++;
-            tempA = tempA->next;
+        ListNode* pA = headA;
+        ListNode* pB = headB;
+
+        while(pA != pB) {
+            if(pA == NULL)
+                pA = headB;
+            else
+                pA = pA->next;
+
+            if(pB == NULL)
+                pB = headA;
+            else
+                pB = pB->next;
         }
-        ListNode *tempB = headB;
-        while(tempB != NULL){
-            countB++;
-            tempB = tempB->next;
-        }
-        tempA = headA;
-        tempB = headB;
-        if((countA-countB) > 0){
-            for(int i=0 ; i<(countA-countB) ; i++){
-                tempA = tempA->next;
-            }
-        }
-        else if((countB-countA) > 0){
-            for(int i=0 ; i<(countB-countA) ; i++){
-                tempB = tempB->next;
-            }
-        }
-        while((tempA != tempB) && (tempA != NULL) && (tempB != NULL)){
-            tempA = tempA->next;
-            tempB = tempB->next;
-        }
-        return tempA;
+
+        return pA;
     }
 };
