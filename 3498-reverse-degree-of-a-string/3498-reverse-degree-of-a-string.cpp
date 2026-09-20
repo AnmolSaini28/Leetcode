a2cf70aea961689d53b55e8c6bf42d09fc5b6c56
Anmol Saini <143955945+AnmolSaini28@users.noreply.github.com>
2026-09-20 12:53:41 +0530

    Time: 3 ms (16.14%), Space: 10.6 MB (5.64%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int sum = 0;
+        int n = s.size();
+        vector <int > arr(n,0);
+        for (int i = 0; i < n; i++) {
+            arr[i] = (26 - (s[i] - 'a')) * (i + 1);
+        }
+        for(int i=0 ; i<n ; i++){
+            sum += arr[i];
+        }
+        return sum;
+    }
+};
